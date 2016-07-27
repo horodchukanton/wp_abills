7c5bc590fad473b4bcb6b4450217c8167d504682
Andrew <shvydko.andrey@mail.ru>
2016-07-27 15:12:55 -0400

    curl to msgs func

--- wp-content/plugins/abills/request_call.php
+++ wp-content/plugins/abills/request_call.php
@@ -9,27 +9,28 @@
 require_once('/var/www/wordpress/wp-load.php');
 
 
-if( $curl = curl_init() ) {
+
     $abills_billing_url    = get_option('abills_billing_url');
     $api_key    = get_option('abills_api_key');
     if(!empty($_POST['phone'])){
         $phone =  $_POST['phone'];
         #$today = date("Y-m-d G:i:s");
 
-        curl_setopt($curl,CURLOPT_URL, "$abills_billing_url/admin/index.cgi");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER , 0);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, "get_index=wordpress_to_msgs&xml=1&API_KEY=$api_key&header=1&PHONE=$phone&REQUEST_CALL=1");
-        $out = curl_exec($curl);
-        curl_close($curl);
+        // curl_setopt($curl,CURLOPT_URL, "$abills_billing_url/admin/index.cgi");
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+        // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER , 0);
+        // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        // curl_setopt($curl, CURLOPT_POST, true);
+        // curl_setopt($curl, CURLOPT_POSTFIELDS, "get_index=wordpress_to_msgs&xml=1&API_KEY=$api_key&header=1&PHONE=$phone&REQUEST_CALL=1");
+        // $out = curl_exec($curl);
+        // curl_close($curl);
+        $url    = "$abills_billing_url/admin/index.cgi";
+        $params = "get_index=wordpress_to_msgs&xml=1&API_KEY=$api_key&header=1&PHONE=$phone&REQUEST_CALL=1";
+
+        $result = curl_to_msgs($url, $params);
 
 
-        $result = new SimpleXMLElement($out);
-
-
-        if(strcasecmp($result[0], 'success') == 0){
+        if(strcasecmp($result, 'success') == 0){
             print 'success';
         }
         return 1;
@@ -47,17 +48,23 @@ if( $curl = curl_init() ) {
 
         $email = $_POST['EMAIL'];
         $message = $_POST['MESSAGE'];
-        curl_setopt($curl,CURLOPT_URL, "$abills_billing_url/admin/index.cgi");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER , 0);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, "get_index=wordpress_to_msgs&xml=1&API_KEY=$api_key&header=1&MESSAGE=$message&EMAIL=$email&REQUEST_MSG=1");
-        $out = curl_exec($curl);
-        curl_close($curl);
+        // curl_setopt($curl,CURLOPT_URL, "$abills_billing_url/admin/index.cgi");
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+        // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER , 0);
+        // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        // curl_setopt($curl, CURLOPT_POST, true);
+        // curl_setopt($curl, CURLOPT_POSTFIELDS, "get_index=wordpress_to_msgs&xml=1&API_KEY=$api_key&header=1&MESSAGE=$message&EMAIL=$email&REQUEST_MSG=1");
+        // $out = curl_exec($curl);
+        // curl_close($curl);
 
-        $result = new SimpleXMLElement($out);
-        if(strcasecmp($result[0], 'success') == 0){
+        // $result = new SimpleXMLElement($out);
+
+        $url    = "$abills_billing_url/admin/index.cgi";
+        $params = "get_index=wordpress_to_msgs&xml=1&API_KEY=$api_key&header=1&MESSAGE=$message&EMAIL=$email&REQUEST_MSG=1";
+
+        $result = curl_to_msgs($url, $params);
+
+        if(strcasecmp($result, 'success') == 0){
             echo "<div class='container'>";
             echo "<div class='row'>";
             echo "<div class='alert alert-success' role='alert'>";
@@ -79,18 +86,23 @@ if( $curl = curl_init() ) {
       $date = $_POST['DATE'];
 
 
-        curl_setopt($curl,CURLOPT_URL, "$abills_billing_url/admin/index.cgi");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER , 0);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, "get_index=wordpress_to_msgs&xml=1&API_KEY=$api_key&header=1&FIO=$fio&PHONE=$phone&ADDRESS=$address&DATE=$date&REGISTRATION=1&language=russian");
-        $out = curl_exec($curl);
-        curl_close($curl);
+        // curl_setopt($curl,CURLOPT_URL, "$abills_billing_url/admin/index.cgi");
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+        // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER , 0);
+        // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        // curl_setopt($curl, CURLOPT_POST, true);
+        // curl_setopt($curl, CURLOPT_POSTFIELDS, "get_index=wordpress_to_msgs&xml=1&API_KEY=$api_key&header=1&FIO=$fio&PHONE=$phone&ADDRESS=$address&DATE=$date&REGISTRATION=1&language=russian");
+        // $out = curl_exec($curl);
+        // curl_close($curl);
 
-        $result = new SimpleXMLElement($out);
+        // $result = new SimpleXMLElement($out);
 
-        if(strcasecmp($result[0], 'success') == 0){
+      $url    = "$abills_billing_url/admin/index.cgi";
+        $params = "get_index=wordpress_to_msgs&xml=1&API_KEY=$api_key&header=1&FIO=$fio&PHONE=$phone&ADDRESS=$address&DATE=$date&REGISTRATION=1&language=russian";
+
+        $result = curl_to_msgs($url, $params);
+
+        if(strcasecmp($result, 'success') == 0){
           echo "<div class='container'>";
           echo "<div class='row'>";
           echo "<div class='alert alert-success' role='alert'>";
@@ -101,8 +113,10 @@ if( $curl = curl_init() ) {
           echo "</div></div></div>";
 
           header("Refresh:5; URL='/'");
+          return 1;
         }
       }
-}
+      
+
 
 ?>
